5e38270473cba83d646473e2b513f2540eda7190
Zachary Pinto <zpinto@uci.edu>
2020-03-18 17:07:30 -0700

    complete elastic search consumer and kafka connector demo

---
+++ kafka-connect/run.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+# run the twitter connector
+connect-standalone connect-standalone.properties twitter.properties
--- kafka-consumer-elasticsearch/src/main/java/com/github/zpinto/kafka/elasticsearch/ElasticSearchConsumer.java
+++ kafka-consumer-elasticsearch/src/main/java/com/github/zpinto/kafka/elasticsearch/ElasticSearchConsumer.java
@@ -1,9 +1,138 @@
 package com.github.zpinto.kafka.elasticsearch;
 
-public class ElasticSearchConsumer {
-    public static void main(String[] args,) {
+import com.google.gson.JsonParser;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ElasticSearchConsumer {
+    public static RestHighLevelClient createClient() {
+        String hostname = "kafka-course-7428368759.us-east-1.bonsaisearch.net";
+        String username = "9rjze9arbq";
+        String password = "yhm08ctm8b";
+
+        // don't do if you run local ES
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder builder = RestClient.builder(
+                new HttpHost(hostname, 443, "https"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+                        return httpAsyncClientBuilder.setDefaultCredentialsProvider((credentialsProvider));
+                    }
+                });
+
+        RestHighLevelClient client = new RestHighLevelClient(builder);
+        return client;
     }
 
-    public static
+    public static KafkaConsumer<String, String> createConsumer(String topic) {
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "kafka-demo-elasticsearch";
+
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        // earliest, latest
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false"); //disable auto commit of offsets
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100"); //number of records on each fetch
+
+
+        // create a consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Arrays.asList(topic));
+        return consumer;
+    }
+    private static final JsonParser jsonParser = new JsonParser();
+    private static String extractIdFromTweet (String json) {
+        // gson library
+        return jsonParser.parse(json).getAsJsonObject()
+                .get("id_str")
+                .getAsString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
+        RestHighLevelClient client = createClient();
+
+        KafkaConsumer<String, String> consumer = createConsumer("twitter_tweets");
+
+        while(true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100)); // new in Kafka 2.0.0
+
+            Integer recordCount = records.count();
+            logger.info("Recieved " + recordCount + " records");
+
+            BulkRequest bulkRequest = new BulkRequest();
+
+            for (ConsumerRecord<String, String> record: records) {
+                // where we insert data into elasticsearch
+                String jsonString = record.value();
+
+                // 2 strategies for creating ids
+                // kafka generic ID
+//                String id = record.topic() + "_" + record.partition() + "_" + record.offset();
+
+                // twitter specific id
+                try {
+                    String id = extractIdFromTweet(jsonString);
+
+                    IndexRequest indexRequest = new IndexRequest("twitter")
+                            .id(id)
+                            .source(jsonString, XContentType.JSON);
+
+                    bulkRequest.add(indexRequest); // we add to our bulk request (takes no time)
+                } catch (NullPointerException e) {
+                    logger.warn("skipping bad data: " + record.value());
+                }
+            }
+
+            if (recordCount > 0) {
+                BulkResponse bulkItemResponse = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+
+                logger.info("Committing offsets...");
+                consumer.commitSync();
+                logger.info("Offsets have been committed");
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // close the client gracefully
+        // client.close();
+    }
 }
